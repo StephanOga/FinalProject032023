4a415678e4f6b4f516f588c131c8f2911e873da6
Stepan <sud_pravo@bk.ru>
2023-03-31 19:28:31 +0700

    Создаем метод , который будет печатать наш массив

--- Program.cs
+++ Program.cs
@@ -12,3 +12,15 @@ void ReplaceElementsMainToSecondArray(string[] arrayMain, string[] secondArray)
     }
   }
 }
+
+void PrintMainAndSecondArray(string[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    {
+      
+      Console.Write(array[i]); 
+    }
+    Console.WriteLine();
+  }
+}
