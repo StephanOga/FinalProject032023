11581afe860795af678cb18fd1eb2617b0a38bde
Stepan <sud_pravo@bk.ru>
2023-03-31 19:27:11 +0700

    Создаём метод, который будет переносить элементы из основного массива во второй массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] arrayMain = new string[4] { "Hello", "2", "world", ":-)" };
 string[] secondArray = new string[arrayMain.Length];
+void ReplaceElementsMainToSecondArray(string[] arrayMain, string[] secondArray)
+{
+  int count = 0;
+  for (int i = 0; i < arrayMain.Length; i++)
+  {
+    if (arrayMain[i].Length <= 3)
+    {
+      secondArray[count] = arrayMain[i];
+      count++;
+    }
+  }
+}
